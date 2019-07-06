8c436fc2306a6f4451926890d95e990208f7c08c
mikerobards <mikerobards@icloud.com>
2019-07-06 14:57:17 -0400

    Updates

---
+++ precache-manifest.1f730d6c16081c3a90b1485c0f9d462d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681e450ae9ae726813078688e8311bdb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "68e8aff12300b5aaf02e",
+    "url": "/monsters-rolodex/static/css/main.58579d82.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "68e8aff12300b5aaf02e",
+    "url": "/monsters-rolodex/static/js/main.2e9808ca.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
